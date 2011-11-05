9931bffad09bb0a0351f8a2a85eae33acfcf8779
invoshare <shared-email@invoscape.com>
2011-11-05 18:57:23 +0530

    Initial commit. Created new rails project - CY

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JukeBox::Application.config.secret_token = '05418fd832eadecb3b396e962264ed731667db9e557e9b687294bae890ac47d350a9c47172f4022e8dc6b5383498847da56e309b3c6ea0b776764af5efe2ec93'
